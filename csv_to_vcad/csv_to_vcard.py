625b32991615d15f8b4d1f8359892816b5e5f780
jmarusak <code@work.com>
2020-04-04 13:55:37 -0400

    yahoo export format

--- csv_to_vcad/csv_to_vcard.py
+++ csv_to_vcad/csv_to_vcard.py
@@ -1,22 +1,34 @@
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
-contacts = pd.read_csv('../../data/csv_to_vcard/Contacts.csv', header=0)
-filtered = contacts[contacts['Category'] == 'Agents'].fillna('')
+contacts = pd.read_csv('../../data/csv_to_vcard/contacts.csv', header=0)
+filtered = contacts[contacts['Job title'] == 'Agent'].fillna('')
+
+prefix = 'Z'
 
 vcards = []
 
-for index, row in filtered[['First', 'Last', 'Company', 'Email', 'Other']].iterrows():
+for index, row in filtered[['Name', 'Employer', 'Email 1', 'Phone 1']].iterrows():
+	first_last_name = row['Name'].split(' ')
+
 	vcards.append('BEGIN:VCARD')
 	vcards.append('VERSION:3.0')
-	vcards.append('N:{};{}'.format(row['Last'], row['First']))
-	vcards.append('FN:{} {}'.format(row['First'], row['Last']))
-	vcards.append('ORG:{}'.format(row['Company']))
-	vcards.append('EMAIL;type=INTERNET;type=WORK;type=pref:{}'.format(row['Email']))
-	if row['Other'] != '':
-		vcards.append('TEL;type=WORK;type=VOICE;type=pref:{}'.format(row['Other']))
+	vcards.append('N:{};{} {}'.format(first_last_name[1], prefix, first_last_name[0]))
+	vcards.append('FN:{} {}'.format(prefix, row['Name']))
+	if row['Employer'] != '':
+		vcards.append('ORG:{}'.format(row['Employer']))
+	vcards.append('EMAIL;type=INTERNET;type=WORK;type=pref:{}'.format(row['Email 1']))
+	if row['Phone 1'] != '':
+		vcards.append('TEL;type=MOBILE;type=VOICE;type=pref:{}'.format(row['Phone 1']))
 	vcards.append('END:VCARD')
 	vcards.append('')
 
-with open('../../data/csv_to_vcard/Contacts.vcf', 'w') as file:
+for vcard in vcards:
+	print(vcard)
+
+now = datetime.now()
+timestamp = now.strftime("%Y%d%m%H%M")
+
+with open('../../data/csv_to_vcard/contacts_{}.vcf'.format(timestamp), 'w') as file:
     file.writelines('{}\n'.format(vcard) for vcard in vcards)
